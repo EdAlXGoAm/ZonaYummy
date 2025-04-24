96d8de70a9ac3d997efdf5a68006ca75f2cee11d
EdAlXGoAm <edalxgoam@gmail.com>
2025-04-24 17:37:35 -0600

    Improving Platillos Selector

--- zy_user_client/src/modules/Ordenes/MeseroPage/OrdersInterface.js
+++ zy_user_client/src/modules/Ordenes/MeseroPage/OrdersInterface.js
@@ -285,15 +285,15 @@ const OrdersInterface = ({ modeInterface }) => {
     };
 
     return (
-        <div className="container-fluid" style={{backgroundColor: (reloadFlag && modeInterface) && ('#ff6d6d')}}>
+        <div className="container-fluid" style={{background: (reloadFlag && modeInterface) ? 'linear-gradient(to right, #e0f7fa, #b2ebf2)' : 'none'}}>
             <div className="row">
                 <div className="col-4">
-                    <div style={{color: '#fff', textAlign:'left'}}>
+                    <div style={{color: '#000000', textAlign:'left'}}>
                         <Counter30_to_0 handleReloadFlag={handleReloadFlag}/>
                     </div>
                 </div>
                 <div className="col-8">
-                    <h1 style={{ color: "#ffffff" }} onDoubleClick={handleDoubleClick}>Comandas</h1><ToastContainer />
+                    <h1 style={{ color: "#000000" }} onDoubleClick={handleDoubleClick}>Comandas</h1><ToastContainer />
                     {/* {modeInterface && (
                         <div>
                             <Button variant="primary" onClick={() => setComandasPerScreen(6)}>6</Button>
--- zy_user_client/src/modules/Ordenes/MeseroPage/PlatilloSelectorComponent.js
+++ zy_user_client/src/modules/Ordenes/MeseroPage/PlatilloSelectorComponent.js
@@ -1,50 +1,13 @@
 import './PlatilloSelector.css';
-import React, { useState, useEffect, useRef } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowCircleLeft, faArrowCircleRight } from '@fortawesome/free-solid-svg-icons';
-
+import React from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const PlatilloSelector = ({addPlatilloToOrder, platillos, numPlatillos}) => {
+const PlatilloSelector = ({addPlatilloToOrder, platillos}) => {
     const notify = (message) => toast(message);
-    const [numPlatilloButtonsPerSlide, setNumPlatilloButtonsPerSlide] = useState (4);
-    const containerRef = useRef(null);
-    const [slide, setSlide] = useState(1);
-    const handleSlideChange = (newSlide) => {
-        const maxSlide = Math.ceil(numPlatillos / numPlatilloButtonsPerSlide);
-        if (newSlide > 0 && newSlide <= maxSlide) {
-            setSlide(newSlide);
-        }
-    };
-    const updateNumPlatilloButtonsPerSlide = (width) => {
-        const buttonWidth = 85;
-        const newButtonsPerSlide = Math.floor(width / buttonWidth)*2;
-        setNumPlatilloButtonsPerSlide(prevNumPlatilloButtonsPerSlide => {
-            return newButtonsPerSlide;
-        });
-    }
-    useEffect(() => {
-        const resizeObserver = new ResizeObserver(entries => {
-            for (let entry of entries) {
-                const { width, height } = entry.contentRect;
-                updateNumPlatilloButtonsPerSlide(width);
-            }
-        });
-        if (containerRef.current) {
-            resizeObserver.observe(containerRef.current);
-        }
-        return () => {
-            if (containerRef.current) {
-                resizeObserver.unobserve(containerRef.current);
-            }
-        };
-    }, []);
     const renderButtons = () => {
         const buttons = [];
-        const start = (slide - 1) * numPlatilloButtonsPerSlide;
-        const end = Math.min(slide * numPlatilloButtonsPerSlide, numPlatillos);
-        for (let i = start; i < end; i++) {
+        for (let i = 0; i < platillos.length; i++) {
             if (platillos[i].Disponibilidad !== 0) {
                 buttons.push(
                     <button key={i} className="platilloButton" onClick={() => addPlatilloToOrder(platillos[i])}>
@@ -65,28 +28,13 @@ const PlatilloSelector = ({addPlatilloToOrder, platillos, numPlatillos}) => {
                     </button>
                 );
             }
-
         }
         return buttons;
     };
 
     return (
-        <div className="row">
-            <div className="col-2 containerArrowPlatillosButton">
-                <button className="buttonArrowPlatillosSelector" onClick={() => handleSlideChange(slide - 1)}>
-                    <FontAwesomeIcon icon={faArrowCircleLeft} size="3x" /> {/* Increased size */}
-                </button>
-            </div>
-            <div className="col-8">
-                <div className="row slider_row_slide" ref={containerRef}>
-                    {renderButtons()}
-                </div>
-            </div>
-            <div className="col-2 containerArrowPlatillosButton">
-                <button className="buttonArrowPlatillosSelector" onClick={() => handleSlideChange(slide + 1)}>
-                    <FontAwesomeIcon icon={faArrowCircleRight} size="3x" /> {/* Increased size */}
-                </button>
-            </div>
+        <div className="platillo-scroll-container">
+            {renderButtons()}
             <ToastContainer />
         </div>
     )
